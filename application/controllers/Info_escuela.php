3676df9af5129e23342a05009837f37a8c8a9338
alex111 <uziel.cap.123@gmail.com>
2018-05-11 17:01:42 -0500

    nuevo controller info escuela

---
+++ application/controllers/Info_escuela.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Info_escuela extends CI_Controller {
+
+		function __construct() {
+			parent::__construct();
+			$this->load->library('Utilerias');
+			$this->load->helper('form');
+		}
+
+		function get_info()
+		{
+
+		}
+
+
+}
