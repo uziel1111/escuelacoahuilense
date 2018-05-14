66b6f0babf4101b4248991777f4427318c16501d
Miguel Hernández <miguellhdez@gmail.com>
2018-05-14 08:46:28 -0500

    index básqueda por lista

--- application/controllers/Busqueda_xlista.php
+++ application/controllers/Busqueda_xlista.php
@@ -2,5 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Busqueda_xlista extends CI_Controller {
+	public function index(){
+ 		echo ":)";
+	}// index()
 
 }// Busqueda_lista
