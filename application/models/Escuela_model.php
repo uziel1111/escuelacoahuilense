c7622ce6c37da2892c69efbf78f4e3377aa0fca0
alex111 <uziel.cap.123@gmail.com>
2018-05-16 11:33:47 -0500

    cambios a models con los nuevos ajustes a la bd

--- application/models/Escuela_model.php
+++ application/models/Escuela_model.php
@@ -5,29 +5,29 @@ class Escuela_model extends CI_Model
         parent::__construct();
     }
 
-    function get_xparams($cve_municipio,$cve_nivel,$cve_sostenimiento,$nombre_escuela){
-      $this->db->select('es.id_cct, es.cve_centro, tu.turno_registro as turno, es.nombre_centro,ne.nivel_educativo,mu.municipio,lo.localidad,es.domicilio');
+    function get_xparams($id_municipio,$id_nivel,$id_sostenimiento,$nombre_escuela){
+      $this->db->select('es.id_cct, es.cve_centro, tu.turno, es.nombre_centro,ni.nivel,sso.subsostenimiento, mo.modalidad,mu.municipio,es.domicilio');
       $this->db->from('escuela as es');
-      $this->db->join('nivel_educativo as ne', 'ne.cve_nivel_educativo = es.cve_nivel_educativo AND es.cve_tipo_educacion = ne.cve_tipo_educacion');
-      // $this->db->join('localidad as lo', 'lo.cve_localidad = es.cve_localidad AND es.cve_region = lo.cve_region AND es.cve_subregion = lo.cve_subregion');
-      $this->db->join('localidad as lo', 'lo.cve_localidad = es.cve_localidad');
-      $this->db->join('municipio as mu', 'mu.cve_municipio = es.cve_municipio');
-      $this->db->join('turno_registro as tu', 'tu.cve_turno = es.cve_turno');
-      $this->db->join('sostenimiento as so', 'so.cve_sostenimiento = es.cve_sostenimiento');
+      $this->db->join('turno as tu', 'es.id_turno = tu.id_turno');
+      $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
+      $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
+      $this->db->join('sostenimiento as so', 'sso.id_sostenimiento = so.id_sostenimiento');
+      $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
+      $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
 
-      if($cve_municipio>=0){
-        $this->db->where('mu.cve_municipio', $cve_municipio);
+      if($id_municipio>0){
+        $this->db->where('es.id_municipio', $id_municipio);
       }
-      if($cve_nivel>=0){
-        $this->db->where('es.cve_nivel_educativo', $cve_nivel);
+      if($id_nivel>0){
+        $this->db->where('es.id_nivel', $id_nivel);
       }
-      if($cve_sostenimiento>=0){
-        $this->db->where('es.cve_sostenimiento', $cve_sostenimiento);
+      if($id_sostenimiento>0){
+        $this->db->where('so.id_sostenimiento', $id_sostenimiento);
       }
       if($nombre_escuela!=''){
         $this->db->like('es.nombre_centro', $nombre_escuela);
       }
-      $this->db->group_by("es.cve_centro");
+      $this->db->group_by("es.id_cct");
       // $this->db->get();
       // $str = $this->db->last_query();
       // echo $str; die();
@@ -35,11 +35,14 @@ class Escuela_model extends CI_Model
     }// get_xparams()
 
     function get_xcvecentro($cve_centro){
-      $this->db->select('es.id_cct,es.cve_centro,es.nombre_centro,tu.turno_registro as turno');
+      $this->db->select('es.id_cct,es.cve_centro,es.nombre_centro,tu.turno');
       $this->db->from('escuela as es');
-      $this->db->join('turno_registro as tu', 'tu.cve_turno = es.cve_turno');
+      $this->db->join('turno as tu', 'es.id_turno = tu.id_turno');
       $this->db->where('es.cve_centro', $cve_centro);
-      $this->db->group_by("tu.cve_turno_registro");
+      $this->db->group_by("tu.id_turno");
+      // $this->db->get();
+      // $str = $this->db->last_query();
+      // echo $str; die();
       return  $this->db->get()->result_array();
     }// get_xcentro()
 
--- application/views/index.php
+++ application/views/index.php
@@ -27,7 +27,3 @@
       </div>
     </div>
   </div>
-<<<<<<< HEAD
-=======
-</div>
->>>>>>> a5ccfbc0394e533477d6f3d04afeb0ece835c012
