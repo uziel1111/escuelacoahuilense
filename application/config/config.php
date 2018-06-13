a2d9266aa7598eab1273aedf9604d175b2fa5932
alex111 <uziel.cap.123@gmail.com>
2018-06-13 10:16:59 -0500

    lo que esta en master

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/proy_completo_coahuila/wicho/escuelacoahuilense/';
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/escuelacoahuilense/';
 
 /*
 |--------------------------------------------------------------------------
