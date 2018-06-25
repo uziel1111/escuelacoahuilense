0202649eed710d11f8da91c9e05a16376ea2a2ae
alex111 <uziel.cap.123@gmail.com>
2018-06-25 09:16:00 -0500

    riego por muni estado

--- application/models/Riesgo_alumn_esc_bim_model.php
+++ application/models/Riesgo_alumn_esc_bim_model.php
@@ -26,18 +26,78 @@ class Riesgo_alumn_esc_bim_model extends CI_Model
 
       $query = $this->db->query("
       SELECT id_cct,
-      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct.") as muyalto_t,
-      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND grado=1) as muyalto_1,
-      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND grado=2) as muyalto_2,
-      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND grado=3) as muyalto_2,
-      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND grado=4) as muyalto_4,
-      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND grado=5) as muyalto_5,
-      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND grado=6) as muyalto_6
+      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND ciclo='".$ciclo."') as muyalto_t,
+      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND ciclo='".$ciclo."' AND grado=1) as muyalto_1,
+      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND ciclo='".$ciclo."' AND grado=2) as muyalto_2,
+      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND ciclo='".$ciclo."' AND grado=3) as muyalto_2,
+      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND ciclo='".$ciclo."' AND grado=4) as muyalto_4,
+      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND ciclo='".$ciclo."' AND grado=5) as muyalto_5,
+      (SELECT SUM(IF(((IF(extraedad>1,1,0)) + (IF(falta_bim".$bimestre.">7, 2,IF(falta_bim".$bimestre.">3, 1,0))) + (IF(cal_esp_bim".$bimestre."<6 AND cal_esp_bim".$bimestre.">0,1,0)) + (IF(cal_mat_bim".$bimestre."<6 and cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND ciclo='".$ciclo."' AND grado=6) as muyalto_6
       FROM riesgo_alumn_esc_bim WHERE id_cct=".$id_cct." AND ciclo='".$ciclo."' GROUP BY id_cct
       ");
         return $query->result_array();
 
     }// get_riesgo_pie_xidct()
+
+    function get_riesgo_pie_xidmuni($id_municipio,$id_nivel,$bimestre,$ciclo){
+      // return  $this->db->get('nivel')->result_array();
+      $var_aux="";
+      if ($id_municipio>0) {
+        $var_aux="esc.id_municipio=".$id_municipio." AND";
+      }
+
+      $query = $this->db->query("
+      SELECT
+      COUNT(rie.curp) total,
+      SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto,
+      SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))=2,1,0)) as alto,
+      SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))=1,1,0)) as medio,
+      SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))=0,1,0)) as bajo
+      FROM riesgo_alumn_esc_bim rie
+      INNER JOIN escuela esc ON rie.id_cct = esc.id_cct
+      WHERE ".$var_aux." esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."'
+      ");
+        return $query->result_array();
+
+    }// get_riesgo_pie_xidmuni()
+
+    function get_riesgo_bar_grados_xidmuni($id_municipio,$id_nivel,$bimestre,$ciclo){
+      // return  $this->db->get('nivel')->result_array();
+
+      if ($id_municipio>0) {
+        $query = $this->db->query("
+        SELECT esc.id_municipio,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_municipio=".$id_municipio." AND esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."') as muyalto_t,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_municipio=".$id_municipio." AND esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=1) as muyalto_1,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_municipio=".$id_municipio." AND esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=2) as muyalto_2,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_municipio=".$id_municipio." AND esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=3) as muyalto_2,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_municipio=".$id_municipio." AND esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=4) as muyalto_4,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_municipio=".$id_municipio." AND esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=5) as muyalto_5,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_municipio=".$id_municipio." AND esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=6) as muyalto_6
+        FROM riesgo_alumn_esc_bim rie
+        INNER JOIN escuela esc ON rie.id_cct = esc.id_cct
+        WHERE esc.id_municipio=".$id_municipio." AND esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' GROUP BY esc.id_municipio
+        ");
+      }
+      else {
+        $query = $this->db->query("
+        SELECT esc.id_municipio,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."') as muyalto_t,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=1) as muyalto_1,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=2) as muyalto_2,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=3) as muyalto_2,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=4) as muyalto_4,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=5) as muyalto_5,
+        (SELECT SUM(IF(((IF(rie.extraedad>1,1,0)) + (IF(rie.falta_bim".$bimestre.">7, 2,IF(rie.falta_bim".$bimestre.">3, 1,0))) + (IF(rie.cal_esp_bim".$bimestre."<6 AND rie.cal_esp_bim".$bimestre.">0,1,0)) + (IF(rie.cal_mat_bim".$bimestre."<6 and rie.cal_mat_bim".$bimestre.">0,1,0)))>2,1,0)) as muy_alto FROM riesgo_alumn_esc_bim rie INNER JOIN escuela esc ON rie.id_cct = esc.id_cct WHERE esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' AND rie.grado=6) as muyalto_6
+        FROM riesgo_alumn_esc_bim rie
+        INNER JOIN escuela esc ON rie.id_cct = esc.id_cct
+        WHERE esc.id_nivel=".$id_nivel." AND rie.ciclo='".$ciclo."' limit 1
+        ");
+      }
+
+        return $query->result_array();
+
+    }// get_riesgo_bar_grados_xidmuni()
 
 
 
--- assets/js/riesgo/funcionalidad_riesgo.js
+++ assets/js/riesgo/funcionalidad_riesgo.js
@@ -7,14 +7,58 @@ function Riesgo(){
 }
 
 $("#btn_buscar_ries_muni").click(function() {
-  var id_minicipio = $("#slt_municipio_ries").val();
-  var id_nivel = $("#slt_nivel_ries").val();
-  var id_bimestre = $("#slt_bimestre_ries").val();
-  var id_ciclo = $("#slt_ciclo_ries").val();
+  let id_minicipio = $("#slt_municipio_ries").val();
+  let id_nivel = $("#slt_nivel_ries").val();
+  let id_bimestre = $("#slt_bimestre_ries").val();
+  let id_ciclo = $("#slt_ciclo_ries").val();
+  var obj_grafica = new Grafica();
 
-   var obj_grafica = new Grafica();
-   obj_grafica.TablaPieGraficaPie();
-   obj_grafica.TablaPieGraficaBarPrimaria();
+	$.ajax({
+    url:  base_url+"riesgo/riesgoxmuni_graf",
+    method: 'POST',
+    data: {'id_minicipio':id_minicipio,'id_nivel':id_nivel,'id_bimestre':id_bimestre,'ciclo':"2017-2018"},
+    beforeSend: function(xhr) {
+      // obj_loader.show();
+  },
+  })
+  .done(function( data ) {
+	// obj_loader.hide();
+	var q1 = parseInt(data.graph_pie_riesgo[0]['muy_alto']);
+		var q2 = parseInt(data.graph_pie_riesgo[0]['alto']);
+		var q3 = parseInt(data.graph_pie_riesgo[0]['medio']);
+		var q4 = parseInt(data.graph_pie_riesgo[0]['bajo']);
+	var t1 = parseInt(data.graph_bar_riesgo[0]['muyalto_1']);
+		var t2 = parseInt(data.graph_bar_riesgo[0]['muyalto_2']);
+		var t3 = parseInt(data.graph_bar_riesgo[0]['muyalto_3']);
+		var t4 = parseInt(data.graph_bar_riesgo[0]['muyalto_4']);
+		var t5 = parseInt(data.graph_bar_riesgo[0]['muyalto_5']);
+		var t6 = parseInt(data.graph_bar_riesgo[0]['muyalto_6']);
+		switch(id_nivel) {
+		case '4':
+							$("#dv_graf_riesgo_mun_zona").empty();
+							$("#dv_tab_riesgo_mun_zona").empty();
+              obj_grafica.TablaPieGraficaPie(q1,q2,q3,q4);
+              obj_grafica.TablaPieGraficaBarPrimaria(t1,t2,t3,t4,t5,t6);
+
+		break;
+		case '5':
+							$("#dv_graf_riesgo_mun_zona").empty();
+							$("#dv_tab_riesgo_mun_zona").empty();
+              obj_grafica.TablaPieGraficaPie(q1,q2,q3,q4);
+              obj_grafica.TablaPieGraficaBarSecundaria(t1,t2,t3);
+		break;
+
+
+		default:
+
+		break;
+
+		}
+  })
+  .fail(function(e) {
+    console.error("Error in "); console.table(e);
+  });
+
   // alert(id_minicipio);
   // obj_riesgo.get_Niveles();
 });
