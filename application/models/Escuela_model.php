b8655c00c21b291cf1fa01e0a00ee5688f13db7d
luis sanchez <luis_sanchez_arenas@hotmail.com>
2018-07-04 12:02:35 -0500

    por cct query

--- application/models/Escuela_model.php
+++ application/models/Escuela_model.php
@@ -37,10 +37,16 @@ class Escuela_model extends CI_Model
     }// get_xparams()
 
     function get_xcvecentro($cve_centro){
-      $this->db->select('es.id_cct,es.cve_centro,es.nombre_centro,tu.turno, es.latitud, es.longitud, es.id_nivel, mu.municipio');
+      $this->db->select('es.id_cct,es.cve_centro,es.nombre_centro,ni.nivel, tu.turno, es.latitud, es.longitud, es.id_nivel, mu.municipio, loc.localidad, s.zona_escolar, so.sostenimiento');
       $this->db->from('escuela as es');
       $this->db->join('turno as tu', 'es.id_turno = tu.id_turno');
+      $this->db->join('nivel as ni', 'es.id_nivel = ni.id_nivel');
+      $this->db->join('subsostenimiento as sso', 'es.id_subsostenimiento = sso.id_subsostenimiento');
+      $this->db->join('sostenimiento as so', 'sso.id_sostenimiento = so.id_sostenimiento');
+      $this->db->join('modalidad as mo', 'es.id_modalidad = mo.id_modalidad');
       $this->db->join('municipio as mu', 'es.id_municipio = mu.id_municipio');
+      $this->db->join('supervision as s', 'es.id_supervision = s.id_supervision');
+      $this->db->join('localidad as loc', 'mu.id_municipio = loc.id_municipio AND es.id_localidad = loc.cve_localidad');
       $this->db->where('es.cve_centro', $cve_centro);
       $this->db->group_by("tu.id_turno");
       // $this->db->get();
