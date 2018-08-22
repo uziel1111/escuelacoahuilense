d64a684fd18a9013533b812084f75dfb7f22161a
luis sanchez <luis_sanchez_arenas@hotmail.com>
2018-08-22 11:32:42 -0500

    modales solucion

--- application/views/info/index.php
+++ application/views/info/index.php
@@ -562,7 +562,7 @@
                 <!-- End Modal -->
 
                 <!-- Modal Apoyos -->
-                <div class="modal fade" id="modal_visor_apoyos_academ" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" >
+                <div class="modal fade" id="modal_visor_apoyos_academ" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" style="overflow-y: scroll;">
                     <div class="modal-dialog modal-md" role="document">
                         <div class="modal-content modal-style-1">
                             <div class="modal-header bgcolor-4">
@@ -580,7 +580,7 @@
                 <!-- End Modal -->
 
 								<!-- Modal Apoyos -->
-                <div class="modal fade" id="modal_visor_apoyos_reactivos" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" >
+                <div class="modal fade" id="modal_visor_apoyos_reactivos" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" style="overflow-y: scroll;">
                     <div class="modal-dialog modal-md" role="document">
                         <div class="modal-content modal-style-1">
                             <div class="modal-header bgcolor-4">
@@ -598,7 +598,7 @@
                 <!-- End Modal -->
 
 								<!-- Modal react -->
-                <div class="modal fade" id="modal_visor_reactivos_zom" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" >
+                <div class="modal fade" id="modal_visor_reactivos_zom" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" style="overflow-y: scroll;">
                     <div class="modal-dialog modal-lg" role="document">
                         <div class="modal-content modal-style-1">
                             <div class="modal-header bgcolor-4">
@@ -616,7 +616,7 @@
                 <!-- End Modal -->
 
 								<!-- Modal Apoyos -->
-                <div class="modal fade" id="modal_visor_pdfc2" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" >
+                <div class="modal fade" id="modal_visor_pdfc2" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" style="overflow-y: scroll;">
                     <div class="modal-dialog modal-md" role="document">
                         <div class="modal-content modal-style-1">
                             <div class="modal-header bgcolor-2">
@@ -634,7 +634,7 @@
                 <!-- End Modal -->
 
 								<!-- Modal Apoyos -->
-                <div class="modal fade" id="modal_visor_pdfc3" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" >
+                <div class="modal fade" id="modal_visor_pdfc3" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" style="overflow-y: scroll;">
                     <div class="modal-dialog modal-md" role="document">
                         <div class="modal-content modal-style-1">
                             <div class="modal-header bgcolor-3">
@@ -652,7 +652,7 @@
                 <!-- End Modal -->
 
 								<!-- Modal Apoyos -->
-                <div class="modal fade" id="modal_visor_material_reactivos" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" >
+                <div class="modal fade" id="modal_visor_material_reactivos" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" style="overflow-y: scroll;">
                     <div class="modal-dialog modal-md" role="document">
                         <div class="modal-content modal-style-1">
                             <div class="modal-header bgcolor-4">
