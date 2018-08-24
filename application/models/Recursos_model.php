d8d16ac648e8b907dc59d081d12eae7825b4e893
alex111 <uziel.cap.123@gmail.com>
2018-08-24 16:50:37 -0500

    sfdf

--- application/models/Recursos_model.php
+++ application/models/Recursos_model.php
@@ -41,6 +41,7 @@ class Recursos_model extends CI_Model
     }
 
     function inserta_url($idreactivo, $url, $idusuario, $idtipo, $titulo, $fuente){
+    	date_default_timezone_set('America/Mexico_City');
     	$fecha= date("Y-m-d H:i:s");
     	$data = array(
 		        'id_reactivo' => $idreactivo,
